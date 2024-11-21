ad2de212f8c82990835d64db85ec46a79b856115
Frankie Zeng <frankie.zengfu@gmail.com>
2024-11-21 23:04:20 +0800

    add node plugin

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,7 +1,40 @@
 import { defineConfig,mergeConfig} from 'vitest/config'
+import path from 'path'
 
-export default mergeConfig({}, defineConfig({
+export const nodejsPolarsDirnamePlugin = () => {
+    const name = 'nodejs-polars-dirname-plugin'
+    return {
+        name,
+        
+        transform(code: string, id: string) {
+            // aim for the node_modules/nodejs-polars/bin/native-polars.js file
+            
+            if (id.endsWith('.node')) {
+                const file = path.basename(id)
+                return `
+                // create a custom require function to load .node files
+                import { createRequire } from 'module';
+                const customRequire = createRequire(import.meta.url)
+
+                // load the .node file expecting it to be in the same directory as the output bundle
+                const content = customRequire('./${file}')
+
+                // export the content straight back out again
+                export default content
+                `
+            }
+            // else return the original code (leave code unrelated to nodejs-polars untouched)
+            return code
+        },
+    }
+}
+export default  defineConfig({
   test: {
-    // ...
-  }
-}))
+    
+    
+  },
+  plugins: [
+    nodejsPolarsDirnamePlugin()
+  ]
+  
+})
