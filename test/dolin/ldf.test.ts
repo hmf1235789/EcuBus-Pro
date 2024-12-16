e867212f5ff1656f067549f9513a964358e94a91
Frankie Zeng <frankie.zengfu@gmail.com>
2024-12-17 06:53:27 +0800

    dev ldf

---
+++ test/dolin/ldf.test.ts
@@ -0,0 +1,25 @@
+import { test } from 'vitest'
+import parse from '../../src/main/dolin/ldfParse'
+import fs from 'fs'
+import path from 'path'
+
+test.skip("ldf parse 2.2",()=>{
+    const ldf=fs.readFileSync(path.join(__dirname,'2.2.ldf'),'utf-8')
+    const r=parse(ldf)
+    console.log(r)
+
+})
+
+test.skip("ldf parse 2.1",()=>{
+    const ldf=fs.readFileSync(path.join(__dirname,'2.1.ldf'),'utf-8')
+    const r=parse(ldf)
+    console.log(r)
+
+})
+
+test("ldf file1",()=>{
+    const ldf=fs.readFileSync(path.join(__dirname,'file1.ldf'),'utf-8')
+    const r=parse(ldf)
+    console.log(r)
+
+})
