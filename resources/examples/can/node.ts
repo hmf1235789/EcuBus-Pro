47074b5a34e2b448f1cc4935d265de15ec4b8214
Frankie Zeng <frankie.zengfu@gmail.com>
2025-04-30 22:04:01 +0800

    restore examples

--- resources/examples/can/node.ts
+++ resources/examples/can/node.ts
@@ -3,7 +3,3 @@ let val = 0
 setInterval(() => {
   setSignal('Model3CAN.VCLEFT_liftgateLatchRequest', val++ % 5)
 }, 1000)
-
-Util.OnVar('tests.t1', ({ value }) => {
-  console.log(`update ${value}`)
-})
